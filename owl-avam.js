078a686ed1cf149b296cb0c20e5c220ec36c15c4
Brian Marriaga <brianmarriaga@gmail.com>
2017-11-16 20:19:07 -0400

    owl-avam.js
    owl-avam.js

---
+++ owl-avam.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+$('.loop').owlCarousel({
+    center: true,
+    items:1,
+    loop:true,
+    margin:10,
+    nav:true
+});
